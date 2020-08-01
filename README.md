510c24173f3eef09046f72398c469948679090a8
Catherine Pedersen <cathy.pedersen@gmail.com>
2020-07-31 20:50:10 -0400

    Finished 3rd assignment

---
+++ README.md
@@ -0,0 +1,9 @@
+Express Drills
+Solution code for Express drills
+
+Drills
+Create a route handler function on the path /sum that accepts two query parameters named a and b and find the sum of the two values. Return a string in the format "The sum of a and b is c". Note that query parameters are always strings so some thought should be given to converting them to numbers.
+
+Create an endpoint /cipher. The handler function should accept a query parameter named text and a one named shift. Encrypt the text using a simple shift cipher also known as a Caesar Cipher. It is a simple substitution cipher where each letter is shifted a certain number of places down the alphabet. So if the shift was 1 then A would be replaced by B, and B would be replaced by C and C would be replaced by D and so on until finally Z would be replaced by A. using this scheme encrypt the text with the given shift and return the result to the client.
+
+To send an array of values to the server via a query string simply repeat the key with different values. For instance, the query string ?arr=1&arr=2&arr=3 results in the query object { arr: [ '1', '2', '3' ] }. Create a new endpoint /lotto that accepts an array of 6 numbers between 1 and 20. The function then randomly generates 6 numbers between 1 and 20. Compare the numbers sent in the query with the randomly generated numbers to determine how many match. If fewer than 4 numbers match respond with the string "Sorry you lose". If 4 numbers match respond with the string "Congratulations, you win a free ticket", if 5 numbers match respond with "Congratulations! You win $100!". If all 6 numbers match respond with "Wow! Unbelievable! You could have won the mega millions!".
--- app.js
+++ app.js
@@ -133,3 +133,76 @@ app.get('/cipher', (req, res) => {
     
     res.status(200).send(cipher);
 });
+
+
+app.get('/lotto', (req, res) => {
+    console.log(req.query);
+    const {numbersArr} = req.query;
+    //validate the values
+    if(!numbersArr) {
+        //input not provided
+        return res.status(400).send('ERROR: Please provide 6 numbers');
+    }
+
+    if(!Array.isArray(numbersArr)) {
+        //input not provided
+        return res.status(400).send('ERROR: Array not found');
+    }
+
+    const userInput = numbersArr
+        .map(num => parseInt(num))
+        .filter(num => !Number.isNaN(num) && (num >= 1 && num <= 20));
+
+    if(userInput.length != 6) {
+        return res.status(400).send("ERROR: Must provide 6 integers between 1 and 20");
+    }
+
+    //Create master list of 20 numbers
+    let lowEnd = 1;
+    let highEnd = 20;
+    let masterList = [];
+    for (let i = lowEnd; i <= highEnd; i++) {
+        masterList.push(i);
+    }
+    console.log(masterList)
+
+    //Randomly choose 6 integers between 1 and 20
+    let winningNums = [];
+    for (let i = 1; i <= 6; i++) {
+        let newNum = Math.floor(Math.random(1, 20) * 10);
+        winningNums.push(newNum);
+    }
+    console.log("Winning Numbers: " + winningNums);
+
+    //Compare winning numbers to user's input, returning array of numbers 
+    //that aren't matches
+    let noMatches = winningNums.filter(num => !userInput.includes(num));
+    console.log("Comparison: " + noMatches);
+
+    let msgToUser;
+    switch(noMatches.length) {
+        case 0:
+            //Everything matches!
+            msgToUser = "Wow! Unbelievable! You could have won the mega millions!";
+            break;
+        case 1:
+            //Missed only 1
+            msgToUser = "Congratulations! You win $100!";
+            break;
+        case 2:
+            //Missed 2
+            msgToUser = "Congratulations, you win a free ticket";
+            break;
+        default:
+            msgToUser = "Sorry, you lose";
+    }
+
+    /*res.json({
+        userInput,
+        winningNums,
+        noMatches,
+        msgToUser
+    });*/
+
+    res.send(msgToUser);
+});
